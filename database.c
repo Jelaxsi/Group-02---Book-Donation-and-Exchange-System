1510a812ad37214c97a8f2e29c4138496f334ce8
piragenthuni <piragenthuni@gmail.com>
2025-05-21 13:21:48 +0530

    database

--- database.c
+++ database.c
@@ -1,52 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <mysql/mysql.h>
+#include <stdio.h>                                                                                   
+#include <stdlib.h>                                                                                  
+#include <mysql/mysql.h>                                                                             
 
-int main() {
-    MYSQL *conn;
-    MYSQL_RES *res;
-    MYSQL_ROW row;
+int main(void)                                                                                       
+{                                                                                                    
+  MYSQL *conn;                                                                                       
 
-    // Initialize
-    conn = mysql_init(NULL);
-    if (conn == NULL) {
-        printf("mysql_init() failed\n");
-        return 1;
-    }
+  if ((conn = mysql_init(NULL)) == NULL)                                                             
+  {                                                                                                  
+    fprintf(stderr, "Could not init DB\n");                                                 
+    return EXIT_FAILURE;                                                                             
+  }                                                                                                  
+  if (mysql_real_connect(conn, "127.0.0.1", "root", "3559", "user", 0, NULL, 0) == NULL)             
+  {                                                                                                  
+    fprintf(stderr, "DB Connection Error\n");                                                        
+    return EXIT_FAILURE;                                                                             
+  } 
+  
+  
 
-    // Connect to MySQL
-    if (mysql_real_connect(conn, "localhost", "root", "3559", "user", 0, NULL, 0) == NULL) {
-        fprintf(stderr, "mysql_real_connect() failed\nError: %s\n", mysql_error(conn));
-        mysql_close(conn);
-        return 1;
-    }
-
-    // Run Query
-    if (mysql_query(conn, "SELECT * FROM books")) {
-        fprintf(stderr, "SELECT failed. Error: %s\n", mysql_error(conn));
-        mysql_close(conn);
-        return 1;
-    }
-
-    // Store & Print Result
-    res = mysql_store_result(conn);
-    if (res == NULL) {
-        fprintf(stderr, "mysql_store_result() failed\n");
-        mysql_close(conn);
-        return 1;
-    }
-
-    int num_fields = mysql_num_fields(res);
-    while ((row = mysql_fetch_row(res))) {
-        for (int i = 0; i < num_fields; i++) {
-            printf("%s ", row[i] ? row[i] : "NULL");
-        }
-        printf("\n");
-    }
-
-    // Clean up
-    mysql_free_result(res);
-    mysql_close(conn);
-
-    return 0;
+  if (mysql_query(conn, "INSERT INTO books VALUES (2,'Hello World')") != 0)                   
+  {                                                                                                  
+    fprintf(stderr, "Query Failure\n");                                                              
+    return EXIT_FAILURE;                                                                             
+  }                                                                                                  
+  
+  
+  mysql_close(conn);                                                                                 
+  return EXIT_SUCCESS;                                                                               
 }
